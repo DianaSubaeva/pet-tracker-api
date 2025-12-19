af07633ec90baa1df7a9363116a0712fd7cd4ccc
DianaSubaeva <dianasubaeva439@gmail.com>
2025-12-20 01:00:06 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,25 +1,11 @@
-// server.js - Рабочая версия для Vercel Serverless
+// server.js - минимальная рабочая версия
 const jsonServer = require('json-server');
-
-// Создаём сервер
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// Настраиваем CORS для фронтенда
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  next();
-});
-
-// Используем middleware
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-
-// Всегда отдаём router (все маршруты)
 server.use(router);
 
-// Экспортируем для Vercel Serverless
+// Экспорт для Vercel
 module.exports = server;
